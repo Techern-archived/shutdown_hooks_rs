4b47c562eedf603ac9159beac1f0e1c5195921fc
Cruz Julian Bishop <cruzjbishop@gmail.com>
2015-07-29 10:28:10 +1000

    Fix spacing in docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
 //! Provides access to shutdown hooks
-//!
+//! 
 //! Currently, only adding new hooks is supported.
-//!
+//! 
 //! An unspecified future release will have a collection of hooks and have the ability to remove them
 
 use std::os::raw::c_int;
 
-///Adds a callback function to be called on exit
+/// Adds a callback function to be called on exit
 #[allow(unused_assignments)] // Assigned in unsafe block
 pub fn add_shutdown_hook(callback: extern fn()) -> bool {
     let mut result: c_int = 0;
